2fa54b495efe14699cacdc76b982e55f8b28c4ee
yh1120 <yonghun1120@gmail.com>
2021-08-24 23:37:28 +0900

    Feat: InputBox, Calendar and Filter(\#9)

--- src/Components/TodoList/TodoCreate.tsx
+++ src/Components/TodoList/TodoCreate.tsx
@@ -1,9 +1,55 @@
 import React from "react";
+import styled from "styled-components";
+
+const TodoHeadBlock = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 52px;
+  padding-bottom: 24px;
+`;
+
+const InsertForm = styled.form`
+  display: flex;
+  background: transparent;
+  padding-left: 40px;
+  padding-top: 16px;
+  padding-right: 60px;
+  padding-bottom: 16px;
+`;
+
+const Input = styled.input`
+  padding: 12px;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid #333;
+  width: 100%;
+  outline: none;
+  font-size: 21px;
+  box-sizing: border-box;
+  color: #119955;
+  &::placeholder {
+    text-align: center;
+    color: #dddddd;
+    font-size: 16px;
+  }
+`;
+
+const Calendar = styled.input`
+  width: 150px;
+  background-color: transparent;
+  border: none;
+  border-bottom: 1px solid #333;
+`;
 
 const TodoCreate: React.FC = () => {
   return (
     <>
-      <div>TodoCreate</div>
+      <TodoHeadBlock>
+        <InsertForm>
+          <Input autoFocus placeholder="What's need to be done?"></Input>
+          <Calendar type="date" />
+        </InsertForm>
+      </TodoHeadBlock>
     </>
   );
 };
--- src/Components/TodoList/TodoFilter.tsx
+++ src/Components/TodoList/TodoFilter.tsx
@@ -1,10 +1,46 @@
 import React from "react";
+import styled from "styled-components";
+
+const FilterLists = [
+  {
+    id: 0,
+    text: "중요도",
+  },
+  {
+    id: 1,
+    text: "최신순",
+  },
+  {
+    id: 2,
+    text: "생성일순",
+  },
+  {
+    id: 3,
+    text: "마감일순",
+  },
+];
+
+const FilterBlock = styled.div`
+  position: relative;
+  left: 100%;
+  padding-left: 20px;
+
+  div {
+    background-color: lightcoral;
+    border: 1px solid #333;
+    width: 90px;
+    text-align: center;
+    margin-bottom: 5px;
+  }
+`;
 
 const TodoFilter: React.FC = () => {
   return (
-    <>
-      <div>TodoFilter</div>
-    </>
+    <FilterBlock>
+      {FilterLists.map(list => (
+        <div key={list.id}>{list.text}</div>
+      ))}
+    </FilterBlock>
   );
 };
 
