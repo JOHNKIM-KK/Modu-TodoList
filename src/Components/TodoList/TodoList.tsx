cb73320f703d4415ecd19ce8bbe45ed6122410e6
yh1120 <yonghun1120@gmail.com>
2021-08-24 23:38:20 +0900

    Feat: List and Item(\#9)

--- src/Components/TodoList/TodoList.tsx
+++ src/Components/TodoList/TodoList.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 import { DragDrop } from "Components/DragDrop";
+import { TodoItem } from "Components/TodoList";
+
+const TodoLists = [
+  {
+    id: 1,
+    taskName: "자소서 쓰기",
+    status: 0,
+    createdAt: "2021-02-03",
+    dueDate: "2021-07-07",
+    importance: 0, // 0~2
+  },
+  {
+    id: 2,
+    taskName: "블로그 쓰기",
+    status: 1,
+    createdAt: "2021-02-03",
+    dueDate: "2021-07-07",
+    importance: 0, // 0~2
+  },
+];
 const TodoList: React.FC = () => {
   return (
-    <>
-      <DragDrop>TodoList</DragDrop>
-    </>
+    <DragDrop>
+      {TodoLists.map(item => (
+        <TodoItem data={item} />
+      ))}
+    </DragDrop>
   );
 };
 
