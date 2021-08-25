d44f2cf3b7d74f17f946e144c97ba6beeb517001
kitebeen <ypyp66@naver.com>
2021-08-25 19:49:28 +0900

    Style(TodoFilter) : 필터 색상 변경 및 애니메이션 추가 (#23)

---
+++ src/Utils/Constants/FilterLists.ts
@@ -0,0 +1,24 @@
+const FilterLists = [
+  {
+    id: 0,
+    text: "중요도",
+    name: "importance",
+  },
+  {
+    id: 1,
+    text: "최신순",
+    name: "recent",
+  },
+  {
+    id: 2,
+    text: "생성일순",
+    name: "createdDate",
+  },
+  {
+    id: 3,
+    text: "마감일순",
+    name: "dueDate",
+  },
+];
+
+export default FilterLists;
